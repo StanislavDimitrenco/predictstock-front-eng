9618f41b5ec1ca364f875ca0e993d5ee21244a03
stanislavdimitrenco <stanislav.dimitrenko@gmail.com>
2021-04-16 23:13:21 +0300

    add table, routers and vuetify

---
+++ src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="home">
+  <v-card-title class="text-center font-weight-black" max-height="600" style="font-size: 36px">Добро пожаловать в панель статистики</v-card-title>
+
+</div>
+</template>
+
+<script>
+export default {
+name: "Welcome"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Users.vue
@@ -0,0 +1,72 @@
+<template>
+
+  <v-card class="mt-5">
+    <v-card-title>Все пользователи</v-card-title>
+    <v-data-table :loading="loading"
+                  :headers="headers"
+                  :items="usersData"
+                  :items-per-page="5"
+                  class="elevation-1"
+                  :footer-props="{
+                  'items-per-page-options': [15, 30, 50, 100]}"
+                  multi-sort
+    >
+      <template v-slot:item.IsPaid="{ item }">
+        <v-chip :color="item.IsPaid ? 'success' : 'error'">
+          {{ item.IsPaid  ? "да" : "нет" }}
+        </v-chip>
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+const axios = require('axios')
+export default {
+  name: 'Users',
+  data() {
+    return {
+      headers: [
+        {
+          text: 'ID',
+          align: 'start',
+          sortable: false,
+          value: 'ID',
+        },
+        {text: 'Telegram ID', value: 'TelegramId'},
+        {text: 'Логин', value: 'Username'},
+        {text: 'Имя', value: 'Name'},
+        {text: 'Фамилия', value: 'Lastname'},
+        {text: 'Дата регистрации', value: 'CreatedAt'},
+        {text: 'Оплаченный', value: 'IsPaid'},
+        {text: 'Оплачен до', value: 'PaidUntil'},
+
+      ],
+      usersData: [],
+      loading: false
+    }
+  },
+  methods: {
+    reformatData: function (data) {
+      return new Date(data).toLocaleString();
+    }
+  },
+  mounted: function () {
+    this.loading = true
+    axios.get('http://localhost:3000/api/users')
+        .then(({data}) => {
+          console.log(data)
+          this.usersData = data.map((user) => ({
+            ...user,
+            PaidUntil: this.reformatData(user.PaidUntil),
+            CreatedAt: this.reformatData(user.CreatedAt),
+          }))
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+        .finally(() => this.loading = false)
+  }
+}
+</script>
+
